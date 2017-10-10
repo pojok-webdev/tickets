93162877ce7d8675068edb05f7fc5c308feff01b
pujie <pw.prayitno@gmail.com>
2017-10-10 16:48:11 +0700

    add footer and menuheader

---
+++ application/views/commons/footer.php
@@ -0,0 +1,8 @@
+<div class="clearfix"></div>
+<footer>
+    <p>
+        <span style="text-align:left;float:left">
+            &copy; 2017 <a href="/" alt="PadiApp">PadiApp</a>
+        </span>
+    </p>
+</footer>
---
+++ application/views/commons/menuheader.php
@@ -0,0 +1,15 @@
+<!-- start: Header -->
+<div class="navbar">
+    <div class="navbar-inner">
+        <div class="container-fluid">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".top-nav.nav-collapse,.sidebar-nav.nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <a class="brand" href="/"><span>PadiApp</span></a>
+            <?php $this->load->view('commons/topmenu');?>
+        </div>
+    </div>
+</div>
+<!-- start: Header -->
