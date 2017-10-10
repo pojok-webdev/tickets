dd7173634b98d26eb9409a79326f3e49c428f9cc
pujie <pw.prayitno@gmail.com>
2017-10-10 16:43:30 +0700

    show ticket duration

--- application/views/tickets/tickets.php
+++ application/views/tickets/tickets.php
@@ -1,103 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<!-- start: Meta -->
-	<meta charset="utf-8">
-    <title><?php echo $title;?></title>
-    <?php $this->load->view("commons/header");?>
-</head>
+<?php $this->load->view("commons/header");?>
 <body>
-	<!-- start: Header -->
-	<div class="navbar">
-		<div class="navbar-inner">
-			<div class="container-fluid">
-				<a class="btn btn-navbar" data-toggle="collapse" data-target=".top-nav.nav-collapse,.sidebar-nav.nav-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</a>
-				<a class="brand" href="index.html"><span>PadiApp</span></a>
-                <?php $this->load->view('commons/topmenu');?>
-			</div>
-		</div>
-	</div>
-	<!-- start: Header -->
-		<div class="container-fluid-full">
-		<div class="row-fluid">
-            <?php $this->load->view('commons/sidemenu');?>
-			<!-- start: Content -->
-			<div id="content" class="span10">
-                <?php $this->load->view('commons/breadcrumb');?>
-			<div class="row-fluid sortable">		
-				<div class="box span12">
-					<div class="box-header" data-original-title>
-						<h2><i class="halflings-icon user"></i><span class="break"></span><?php echo $title;?></h2>
-						<div class="box-icon">
-                            <a href="#" class="btn-close"><i class="halflings-icon plus"></i></a>
-						</div>
+	<?php $this->load->view('commons/menuheader');?>
+	<div class="container-fluid-full">
+	<div class="row-fluid">
+		<?php $this->load->view('commons/sidemenu');?>
+		<!-- start: Content -->
+		<div id="content" class="span10">
+			<?php $this->load->view('commons/breadcrumb');?>
+		<div class="row-fluid sortable">		
+			<div class="box span12">
+				<div class="box-header" data-original-title>
+					<h2><i class="halflings-icon user"></i><span class="break"></span><?php echo $title;?></h2>
+					<div class="box-icon">
+						<a href="#" class="btn-close"><i class="halflings-icon plus"></i></a>
 					</div>
-					<div class="box-content">
-						<table class="table table-striped table-bordered bootstrap-datatable datatable" id="tickets">
-						  <thead>
-							  <tr>
-								  <th width='5%'>Kode</th>
-								  <th width='45%'>Nama</th>
-								  <th width='5%'>Layanan</th>
-								  <th width='5%'>Status</th>
-								  <th>Start</th>
-								  <th width='5%'>Durasi</th>
-								  <th width='5%'>Tanggal Request</th>
-								  <th width='5%'>Pelapor</th>
-								  <th width='5%'>TS</th>
-								  <th width='20%'>Actions</th>
-							  </tr>
-						  </thead>   
-						  <tbody>
-                              <?php foreach($objs as $obj){?>
+				</div>
+				<div class="box-content">
+					<table class="table table-striped table-bordered bootstrap-datatable datatable" id="tickets">
+						<thead>
 							<tr>
-								<td><?php echo $obj->kdticket;?></td>
-								<td class="center"><?php echo $obj->clientname;?></td>
-								<td class="center"><?php echo $obj->service;?></td>
-								<td class="center">
-									<span class="label label-success <?php echo $obj->statuscolor;?>"><?php echo $obj->status;?></span>
-								</td>
-								<td class='start'><?php echo $obj->ticketstart;?></td>
-								<td class='duration' start='<?php echo $obj->ticketstart;?>'></td>
-								<td class="center"><?php echo $obj->create_date;?></td>
-								<td class="center"><?php echo $obj->reporter;?></td>
-								<td class="center">
-									<span><?php echo $obj->createuser;?></span>
-								</td>
-								<td class="center">
-									<a class="btn btn-success" href="#">
-										<i class="halflings-icon white zoom-in"></i>  
-									</a>
-									<a class="btn btn-info" href="#">
-										<i class="halflings-icon white edit"></i>  
-									</a>
-									<a class="btn btn-danger" href="#">
-										<i class="halflings-icon white trash"></i> 
-									</a>
-								</td>
-                            </tr>
-                              <?php }?>
-						  </tbody>
-					  </table>            
-					</div>
-				</div><!--/span-->
-			</div><!--/row-->
-    	</div><!--/.fluid-container-->
-			<!-- end: Content -->
-		</div><!--/#content.span10-->
-        </div><!--/fluid-row-->
-        <?php $this->load->view('tickets/modals');?>
-	<div class="clearfix"></div>
-	<footer>
-		<p>
-			<span style="text-align:left;float:left">&copy; 2017 <a href="padiApp" alt="PadiApp">PadiApp</a></span>
-		</p>
-	</footer>
+								<th width='5%'>Kode</th>
+								<th width='35%'>Nama</th>
+								<th width='5%'>Layanan</th>
+								<th width='5%'>Status</th>
+								<th>Start</th>
+								<th width='5%'>Durasi</th>
+								<th width='5%'>Tanggal Request</th>
+								<th width='5%'>Pelapor</th>
+								<th width='5%'>TS</th>
+								<th width='30%'>Actions</th>
+							</tr>
+						</thead>   
+						<tbody>
+							<?php foreach($objs as $obj){?>
+						<tr>
+							<td><?php echo $obj->kdticket;?></td>
+							<td class="center clientname"><?php echo $obj->clientname;?></td>
+							<td class="center"><?php echo $obj->service;?></td>
+							<td class="center">
+								<span class="label label-success <?php echo $obj->statuscolor;?>"><?php echo $obj->status;?></span>
+							</td>
+							<td class='start'><?php echo $obj->ticketstart;?></td>
+							<td class='duration' start='<?php echo $obj->ticketstart;?>'></td>
+							<td class="center"><?php echo $obj->create_date;?></td>
+							<td class="center"><?php echo $obj->reporter;?></td>
+							<td class="center">
+								<span><?php echo $obj->createuser;?></span>
+							</td>
+							<td class="center">
+								<a class="btn btn-success" href="#">
+									<i class="halflings-icon white zoom-in"></i>  
+								</a>
+								<a class="btn btn-info" href="#">
+									<i class="halflings-icon white edit"></i>  
+								</a>
+								<a class="btn btn-danger" href="#">
+									<i class="halflings-icon white trash"></i> 
+								</a>
+							</td>
+						</tr>
+							<?php }?>
+						</tbody>
+					</table>            
+				</div>
+			</div><!--/span-->
+		</div><!--/row-->
+	</div><!--/.fluid-container-->
+		<!-- end: Content -->
+	</div><!--/#content.span10-->
+	</div><!--/fluid-row-->
+	<?php $this->load->view('tickets/modals');?>
+	<?php $this->load->view('commons/footer');?>
 	<?php $this->load->view("commons/js.php");?>
+	<script type='text/javascript' src='/js/padi.commons.js'></script>
 	<script type='text/javascript' src='/js/tickets/tickets.js'></script>
 </body>
 </html>
--- js/tickets/tickets.js
+++ js/tickets/tickets.js
@@ -1,36 +1,51 @@
 (function($){
-    jsDate = function(options){
-        var settings = $.fn.extend({
-            sqlDate:'',
+    $.fn.showDuration = function(options){
+        var settings = $.extend({
+            startDate:'',
         },options);
-        obj = sqlDate.split(' ');
-        dtstr = obj[0].split('-');
-        year = dtstr[0];
-        month = dtstr[1] - 1;
-        day = dtstr[2];
-        tmstr = obj[1].split(':');
-        hour = tmstr[0];
-        minute = tmstr[1];
-        second = tmstr[2];
-        return new Date(year,month,day,hour,minute,second);
-    }    
+        that = $(this);
+        arr = settings.startDate.split(' ');
+        dtarr = arr[0].split('-');
+        tmarr = arr[1].split(':');
+        start = new Date(dtarr[0],dtarr[1],dtarr[2],tmarr[0],tmarr[1],tmarr[2]);
+        current = new Date();
+        timediff = current - start;
+        hari = Math.ceil(timediff/(1000*60*60*24));
+        shari = Math.ceil(timediff%(1000*60*60*24));
+        jam = Math.ceil(shari/(1000*60*60));
+        sjam = Math.ceil(shari%(1000*60*60));
+        menit = Math.ceil(sjam/(1000*60));
+        smenit = Math.ceil(sjam%(1000*60));
+        detik = Math.ceil(smenit/1000);
+        tahun = Math.ceil(timediff/(1000*60*60*24*365));
+        that.html(hari+' hari '+jam+' jam '+menit+' menit '+detik+' detik');
+    }
     $("#tickets").dataTable({
         "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
         "sPaginationType": "bootstrap",
         "oLanguage": {
-        "sLengthMenu": "_MENU_ records per page"
-        }
+        "sLengthMenu": "_MENU_ records per page",
+        },
+        "aoColumns":
+        [
+            {"bVisible":true},
+            {"bVisible":true},
+            {"bVisible":true},
+            {"bVisible":true},
+            {"bVisible":false},
+            {"bVisible":true},
+            {"bVisible":true},
+            {"bVisible":true},
+            {"bVisible":true},
+            {"bVisible":true}
+        ]
     });
-    $('#tickets .duration').html('test');
     var x = 0;
     setInterval(function(){
-        curDate = new Date();
-        x += 1;
-        $('#tickets tr').each(function(){
-            that = $(this);
-            start = that.find('.start').html();
-            start_ = jsDate(that.find('.start').html());
-            that.find('.duration').html(start+' '+start+' '+curDate);
+        $('#tickets .duration').each(function(){
+            $(this).showDuration({
+                startDate:$(this).attr('start')
             });
-    },1000);
+        });
+    },3000);
 }(jQuery))
