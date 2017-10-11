b525b24ab17805769b95ff2d03ba216fcab9b55c
pujie <pw.prayitno@gmail.com>
2017-10-11 14:23:32 +0700

    add reserve modal

---
+++ application/views/tickets/modaltemplate.php
@@ -0,0 +1,107 @@
+<div class="modal hide fade" id="mdl-template">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">×</button>
+        <h3>Penambahan Ticket</h3>
+    </div>
+    <div class="modal-body">
+        <div class="row-fluid">
+        <div class="box black span4" onTablet="span6" onDesktop="span4">
+        <div class="box-header">
+            <h2><i class="halflings-icon white list"></i><span class="break"></span>Kolom 1</h2>
+            <div class="box-icon">
+                <a href="#" class="btn-minimize"><i class="halflings-icon white chevron-up"></i></a>
+                <a href="#" class="btn-close"><i class="halflings-icon white remove"></i></a>
+            </div>
+        </div>
+        <div class="box-content">
+            <ul class="dashboard-list metro">
+                <li>
+                    <a href="#">
+                        <i class="icon-arrow-up green"></i>                               
+                        <strong>92</strong>
+                        New Comments                                    
+                    </a>
+                </li>
+              <li>
+                <a href="#">
+                  <i class="icon-comment yellow"></i>
+                  <strong>256</strong>
+                  User reviews                                    
+                </a>
+              </li>
+            </ul>
+        </div>
+    </div><!--/span-->
+    
+    <div class="box black span4" onTablet="span6" onDesktop="span4">
+        <div class="box-header">
+            <h2><i class="halflings-icon white user"></i><span class="break"></span>Kolom 2</h2>
+            <div class="box-icon">
+                <a href="#" class="btn-minimize"><i class="halflings-icon white chevron-up"></i></a>
+                <a href="#" class="btn-close"><i class="halflings-icon white remove"></i></a>
+            </div>
+        </div>
+        <div class="box-content">
+            <ul class="dashboard-list metro">
+                <li class="green">
+                    <a href="#">
+                        <img class="avatar" alt="Dennis Ji" src="img/avatar.jpg">
+                    </a>
+                    <strong>Name:</strong> Dennis Ji<br>
+                    <strong>Since:</strong> Jul 25, 2012 11:09<br>
+                    <strong>Status:</strong> Approved             
+                </li>
+                <li class="yellow">
+                    <a href="#">
+                        <img class="avatar" alt="Bill" src="img/avatar.jpg">
+                    </a>
+                    <strong>Name:</strong> Dennis Ji<br>
+                    <strong>Since:</strong> Jul 25, 2012 11:09<br>
+                    <strong>Status:</strong> Pending                                
+                </li>
+            </ul>
+        </div>
+    </div><!--/span-->
+    
+    <div class="box black span4 noMargin" onTablet="span12" onDesktop="span4">
+        <div class="box-header">
+            <h2><i class="halflings-icon white check"></i><span class="break"></span>Kolom 3</h2>
+            <div class="box-icon">
+                <a href="#" class="btn-setting"><i class="halflings-icon white wrench"></i></a>
+                <a href="#" class="btn-minimize"><i class="halflings-icon white chevron-up"></i></a>
+                <a href="#" class="btn-close"><i class="halflings-icon white remove"></i></a>
+            </div>
+        </div>
+        <div class="box-content">
+            <div class="todo metro">
+                <ul class="todo-list">
+                    <li class="red">
+                        <a class="action icon-check-empty" href="#"></a>	
+                        Windows Phone 8 App 
+                        <strong>today</strong>
+                    </li>
+                    <li class="red">
+                        <a class="action icon-check-empty" href="#"></a>
+                        New frontend layout
+                        <strong>today</strong>
+                    </li>
+                    <li class="yellow">
+                        <a class="action icon-check-empty" href="#"></a>
+                        Hire developers
+                        <strong>tommorow</strong>
+                    </li>
+                    <li class="yellow">
+                        <a class="action icon-check-empty" href="#"></a>
+                        Windows Phone 8 App
+                        <strong>tommorow</strong>
+                    </li>
+                </ul>
+            </div>	
+        </div>
+    </div>
+    </div>
+    <div class="modal-footer">
+        <a href="#" class="btn" data-dismiss="modal">Close</a>
+        <a href="#" class="btn btn-primary">Save changes</a>
+    </div>
+</div>
